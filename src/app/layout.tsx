2ddb9147ac7243e171d7a44bb31d6775dbd66d28
Vorium1 <bonzao185@gmail.com>
2026-09-17 13:06:11 -0300

    feat(ux2): load review workspace styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Sora } from "next/font/google";
 import "./globals.css";
 import "./ux2.css";
 import "./ux2-intake.css";
+import "./ux2-review.css";
 
 // Tipografia oficial do Guia de Marca (docs/brand/Guia_de_Marca_Raiz_Digital.pdf):
 // Sora para títulos/institucional, Inter para o restante da plataforma.
