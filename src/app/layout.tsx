795f3ec8675b704bc2d8b0afc85792223b8c623d
Vorium1 <bonzao185@gmail.com>
2026-09-17 17:21:38 -0300

    feat(ux3): load simple talhoes styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import "./ux2-concept.css";
 import "./ux2-concept-detail.css";
 import "./ux2-concept-shell-fixes.css";
 import "./ux3-simple.css";
+import "./ux3-simple-fields.css";
 
 // Tipografia oficial do Guia de Marca (docs/brand/Guia_de_Marca_Raiz_Digital.pdf):
 // Sora para títulos/institucional, Inter para o restante da plataforma.
