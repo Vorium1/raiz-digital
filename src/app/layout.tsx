2f6535b4e74194fb077c757ad6698481b725a247
Vorium1 <bonzao185@gmail.com>
2026-09-17 17:22:26 -0300

    feat(ux3): load simple review styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import "./ux2-concept-detail.css";
 import "./ux2-concept-shell-fixes.css";
 import "./ux3-simple.css";
 import "./ux3-simple-fields.css";
+import "./ux3-simple-review.css";
 
 // Tipografia oficial do Guia de Marca (docs/brand/Guia_de_Marca_Raiz_Digital.pdf):
 // Sora para títulos/institucional, Inter para o restante da plataforma.
