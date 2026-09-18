7d26467f439b5dda5f914e859810e5b3a2388291
Vorium1 <bonzao185@gmail.com>
2026-09-18 09:30:04 -0300

    style(ux3): load readability overrides last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ import "./ux3-analysis.css";
 import "./ux3-send.css";
 import "./ux3-result-document.css";
 import "./ux3-context.css";
+import "./ux3-accessibility.css";
 
 // Tipografia oficial do Guia de Marca (docs/brand/Guia_de_Marca_Raiz_Digital.pdf):
 // Sora para títulos/institucional, Inter para o restante da plataforma.
